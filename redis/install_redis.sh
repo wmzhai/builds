6467d27d86f071d1a37e285cd2d172ccd51c6de3
Weiming Zhai <wmzhai@gmail.com>
2016-09-28 10:19:44 +0800

    redis

---
+++ redis/install_redis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get install redis-server
+
+
+## 检查Redis服务器系统进程
+#~ ps -aux|grep redis
+
+## 通过启动命令检查Redis服务器状态
+#~ netstat -nlt|grep 6379
+
+## 通过启动命令检查Redis服务器状态
+#~ sudo /etc/init.d/redis-server status
+
